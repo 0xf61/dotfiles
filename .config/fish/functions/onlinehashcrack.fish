0b9bc216d0b317aa2028e3080dee06aa08762685
0xf61 <emirhan@kurt.link>
2025-08-27 23:35:37 +0300

    clean fish

--- .config/fish/functions/onlinehashcrack.fish
+++
@@ -1,3 +0,0 @@
-function onlinehashcrack
-    curl -X POST -F "email=test@mail.com" -F "file=@"$argv https://api.onlinehashcrack.com --progress-bar
-end
--- .config/fish/functions/stu.fish
+++
@@ -1,3 +0,0 @@
-function stu
-	curl -F'file=@'$argv https://0x0.st
-end
